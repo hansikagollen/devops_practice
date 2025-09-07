b9a868d6bdcf012929d32ad1a34ff4f35f83b301
hansikagollen <146338862+hansikagollen@users.noreply.github.com>
2025-09-07 12:32:27 +0530

    linked list code

---
+++ LinkedListDemo.java
@@ -0,0 +1,83 @@
+// LinkedListDemo.java
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    // Insert at beginning
+    void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Insert at end
+    void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Insert at a given position (1-based index)
+    void insertAtPosition(int data, int position) {
+        if (position <= 0) {
+            System.out.println("Invalid position!");
+            return;
+        }
+        if (position == 1) {
+            insertAtBeginning(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        for (int i = 1; i < position - 1; i++) {
+            if (temp == null) {
+                System.out.println("Position out of bounds!");
+                return;
+            }
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    // Print linked list
+    void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insertAtEnd(10);
+        list.insertAtBeginning(5);
+        list.insertAtEnd(15);
+        list.insertAtPosition(12, 3);
+
+        System.out.println("Linked List:");
+        list.printList();
+    }
+}
